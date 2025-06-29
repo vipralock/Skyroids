c81c22fd8bcd767f9e1af15fa84313599df5fc59
vipralock <zearishblox@gmail.com>
2025-06-29 06:00:52 -0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,15 @@
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+
+local placeIdToCheck = 283721918
+local localPlayer = Players.LocalPlayer
+
+if game.PlaceId ~= placeIdToCheck then
+	queue_on_teleport([[
+		loadstring(game:HttpGet("https://github.com/vipralock/Skyroids/raw/refs/heads/main/SkyroidsLoadstring.lua", true))()
+	]])
+	TeleportService:Teleport(placeIdToCheck, localPlayer)
+end
+
+
 loadstring(game:HttpGet("https://github.com/vipralock/Skyroids/raw/refs/heads/main/SkyroidsLoadstring.lua",true))()
